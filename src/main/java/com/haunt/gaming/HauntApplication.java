c0ba04a43f3cf8277eadd40ee295d9e87a8249f4
Guilherme Rodrigues <guilhermerdgm@gmail.com>
2020-08-20 18:05:11 -0300

    projeto criado

---
+++ src/main/java/com/haunt/gaming/HauntApplication.java
@@ -0,0 +1,13 @@
+package com.haunt.gaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HauntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HauntApplication.class, args);
+	}
+
+}
