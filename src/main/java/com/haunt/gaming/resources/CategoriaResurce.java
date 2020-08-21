eccc2bd1bd380ee64b7976664674a677076ddba7
Guilherme Rodrigues <guilhermerdgm@gmail.com>
2020-08-21 20:05:51 -0300

    Primeira Classe De Dominio Teste

--- src/main/java/com/haunt/gaming/resources/CategoriaResurce.java
+++ src/main/java/com/haunt/gaming/resources/CategoriaResurce.java
@@ -1,15 +1,30 @@
 package com.haunt.gaming.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.haunt.gaming.domain.Categoria;
 
 @RestController 
 @RequestMapping(value="/categorias")
 public class CategoriaResurce {
 	 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcinando";
+	public List<Categoria> listar() {
+		
+	
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "escritório");
+		
+		List <Categoria> Lista = new ArrayList<>();
+		Lista.add(cat1);
+		Lista.add(cat2);
+		
+		
+		return Lista;
 	}
 }
