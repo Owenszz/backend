e777f54b231be3ae9be482cde247a13b3e697b64
Guilherme Rodrigues <guilhermerdgm@gmail.com>
2020-08-24 20:57:35 -0300

    Criando operaçao de instanciação

--- src/main/java/com/haunt/gaming/HauntApplication.java
+++ src/main/java/com/haunt/gaming/HauntApplication.java
@@ -1,13 +1,34 @@
 package com.haunt.gaming;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class HauntApplication {
+import com.haunt.gaming.domain.Categoria;
+import com.haunt.gaming.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class HauntApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriarepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HauntApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "informática");
+		Categoria cat2 = new Categoria(null, "escritório");
+		
+	
+		categoriarepository.saveAll(Arrays.asList(cat1,cat2));
+	}
+
+	
 }
